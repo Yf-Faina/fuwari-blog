cb9d9524e636182a7283b9c9a304b442b35bb26d
Yf-Faina <1654289166@qq.com>
2025-09-10 17:17:05 +0800

    FIX: Action自动部署

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,8 +1,6 @@
 import rss from "@astrojs/rss";
 import MarkdownIt from "markdown-it";
-
-const sanitizeHtml = require("sanitize-html");
-
+import sanitizeHtml from "sanitize-html";
 import { getSortedPosts } from "@utils/content-utils";
 import { url } from "@utils/url-utils";
 import { siteConfig } from "@/config";
